f96a8dc4482a3d44cf555c32821ef7be20e88b46
ShubhamPal19 <77537775+ShubhamPal19@users.noreply.github.com>
2021-04-02 17:26:31 +0530

    Update file.c

--- codeblocks/bitmasking/file.c
+++ codeblocks/bitmasking/file.c
@@ -1,1 +1,28 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char sre[11];
+    cin>>sre;
+    int klen=strlen(sre),c=0,d=0;
+    for(int i=1;i<=klen-1;i++)// combination till len-1 is must covered soo count it
+        c+=pow(2,i);
+
+    int rer=pow(2,klen);// combination possible  of lenth =len
+  
+    int j=0;
+    while(j<klen)
+    {
+        if(sre[j]-'0'==7)// caculating  
+        {
+           
+            d+=rer/2;
+           
+        }
+        rer/=2;
+        j++;
+    }
+    cout<<c+d+1;
+    return 0;
+}
