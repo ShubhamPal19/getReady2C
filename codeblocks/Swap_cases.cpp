83011fd0b4a6d62b5e7a422d2b471723535a0d68
ShubhamPal19 <shubhampal2309@gmail.com>
2021-04-26 16:20:22 +0530

    Create Swap_cases.cpp

---
+++ codeblocks/Swap_cases.cpp
@@ -0,0 +1,54 @@
+// Write a program for swaping cases in a string S. For swap case necessary conditions are:-
+
+// Remove spaces and any special character
+// Remove digits
+// Convert uppercase to lowercase and vice versa
+// Input Format
+// The first line contains a string S.
+
+// Constraints
+// Output Format
+// Display a single line output of the resulting string after swapping cases.
+
+// Sample Input
+// There are 206 bones
+// Sample Output
+// tHEREAREBONES
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string swap_case(string str)
+{
+  string s = "";
+  char ch;
+  int i, l;
+  l = str.length();
+  for (i = 0; i < l; i++)
+  {
+    ch = str[i];
+    if (ch >= 'a' && ch <= 'z')
+      ch = ch - 32;
+    else if (ch >= 'A' && ch <= 'Z')
+      ch = ch + 32;
+    else
+      continue;
+    cout << s << endl;
+    s.push_back(ch);
+  }
+  cout << "\n"
+       << s;
+  return s;
+}
+
+int main()
+{
+
+  string str;
+  cout << ("\nEnter the string : ");
+  getline(cin, str);
+  cout << str;
+  str = swap_case(str);
+  cout << str;
+}
