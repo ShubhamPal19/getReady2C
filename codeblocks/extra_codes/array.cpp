858f7a043261a81f4eb13555c837a8f38ec284db
ShubhamPal19 <shubhampal2309@gmail.com>
2021-05-26 10:44:00 +0530

    Create array.cpp

---
+++ codeblocks/extra_codes/array.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+
+    int a[3][2]={1,2,3,4,5,6};
+    cout<<a[1][2];
+
+
+
+    // int b[][3]= {                 // column should be mentioned during declaration, while rows can be left blank; 
+
+    //     {1,2,3},
+    //     {43,34},
+    //     {4,5,6}
+    // };
+
+    // for (size_t i = 0; i < 3; i++)
+    // {
+    //    for (size_t j = 0; j < 3; j++)
+    //    {
+    //        cout <<b[i][j]<< " ";
+
+    //    }
+
+    //    cout << endl;
+       
+    // }
+    int b[][4]= {          
+        {1,2,3,32},
+        {43,34,4,1},
+        {4,5,6,9}
+    };
+
+
+   while()
+    
+    
+}
