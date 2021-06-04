f131aae2588defcc6dade650c2d11af406801974
ShubhamPal19 <shubhampal2309@gmail.com>
2021-06-04 21:04:11 +0530

    Create tempCodeRunnerFile.cpp

---
+++ cb_class_codes/data_structure/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+tail=head;
+
+// node * current=head, *previous=NULL,* temp=NULL;
+
+// while (current==NULL)
+// {
+//     temp=current->next;
+//     current->next=previous;
+//     previous=current;
+//     current=temp;
+// }
+// head=previous;
+
