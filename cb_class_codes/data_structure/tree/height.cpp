05f7895a657ad15059c3a6af0c251c83aa8de8e6
ShubhamPal19 <shubhampal2309@gmail.com>
2021-06-05 18:08:15 +0530

    Create height.cpp

---
+++ cb_class_codes/data_structure/tree/height.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename t>
+
+class node
+{
+    public:
+        t data;
+        node<t> * left;
+        node<t> * right;
+
+        node(t d)
+        {     
+            data= d;
+            left=NULL;
+            right=NULL;                            
+
+        }
+
+};
+
+int i=0;
+
+void buildtree(int array[], node<int> *&root)
+{
+   if(array[i]==-1)
+   {
+       return;
+   }
+    node<int> * n= new node<int>(array[i]);
+    
+    root = n;
+    
+    i++;
+    buildtree(array,root->left);
+    i++;
+    buildtree(array,root->right);
+}
+
+int height(node<int> * root)
+{
+    if (root==NULL)
+    {
+        return 0;
+    }
+    return max(height(root->right),height(root->left)) +1;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    // int n;
+    // n=19;
+    // int * array=new int[n];
+    int  array[19]={8,3,1,-1,-1,6,4,-1,-1,7,-1,-1,16,-1,14,13,-1,-1,-1};
+    node<int> * tree;
+    tree=NULL;
+    buildtree(array,tree);
+    cout<<"height of btree is "<<height(tree)<<endl;
+    return 0;
+}
