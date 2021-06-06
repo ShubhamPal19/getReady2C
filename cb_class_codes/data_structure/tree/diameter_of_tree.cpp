7297db8f88b85722a28471c9d9d9fd7142772b2b
ShubhamPal19 <shubhampal2309@gmail.com>
2021-06-06 15:30:56 +0530

    Update diameter_of_tree.cpp

--- cb_class_codes/data_structure/tree/diameter_of_tree.cpp
+++ cb_class_codes/data_structure/tree/diameter_of_tree.cpp
@@ -47,25 +47,107 @@ int height(node<int> *root)
 }
 
 int diameter(node<int> *root)
-{   
+{
 
     if (root == NULL)
     {
         return 0;
     }
 
-    return max(height(root->right)+height(root->left),max(diameter(root->right),diameter(root->left)));
+    return max(height(root->right) + height(root->left), max(diameter(root->right), diameter(root->left)));
 }
+void printin(node<int> *tree) // inorder
+{
+    if (tree == NULL)
+    {
+        return;
+    }
 
+    printin(tree->left);
+    cout << tree->data << "  ";
+    printin(tree->right);
+}
+void printpost(node<int> *tree) // post order
+{
+    if (tree == NULL)
+    {
+        return;
+    }
+
+    printpost(tree->left);
+    printpost(tree->right);
+    cout << tree->data << "  ";
+}
+void printpre(node<int> *tree) // preeorder
+{
+    if (tree == NULL)
+    {
+        return;
+    }
+
+    cout << tree->data << "  ";
+    printpre(tree->left);
+    printpre(tree->right);
+}
 int main(int argc, char const *argv[])
 {
     // int n;
     // n=19;
     // int * array=new int[n];
-    int array[19] = {8, 3, 1, -1, -1, 6, 4, -1, -1, 7, -1, -1, 16, -1, 14, 13, -1, -1, -1};
-    node<int> *tree;
-    tree = NULL;
-    buildtree(array, tree);
-    cout << "height of btree is " << diameter(tree) << endl;
-    return 0;
+    int array[] = {1, 3, 8, 1, 11, 12, 13, 1, -1, -1, 2, 3, -1, -1, 4, -1, -1, -1, -1, -1, -1, 9, -1, 3, -1, 2, -1, 1, 19, -1, -1, 14, -1, -1,
+                   2, 7, -1, -1, 4, -1, -1};
+
+    int array2[] = {1, 3, 8, 1, 11, 12, 13, 1, -1, -1, 2, 3, -1, -1, 4, -1, -1, -1, -1, -1, -1, 9, -1, 3, -1, -1,
+        2, 7, -1, -1, 4, -1, -1};
+node<int> *tree;
+tree = NULL;
+
+buildtree(array, tree);
+cout << "\npost   ";
+printpost(tree);
+cout << "\npre    ";
+printpre(tree);
+cout << "\nin     ";
+printin(tree);
+
+cout << "\nDIAMETER of btree is " << diameter(tree) << endl;
+
+tree = NULL;
+i=0;
+buildtree(array2, tree);
+cout << "\npost   ";
+printpost(tree);
+cout << "\npre    ";
+printpre(tree);
+cout << "\nin     ";
+printin(tree);
+
+cout << "\nDIAMETER of btree is " << diameter(tree) << endl;
+return 0;
 }
+
+//                                     1
+//                               3            2
+//                             8    9         7   4
+//                            1       3
+//                         11           2
+//                         12             1
+//                        13           19   14
+//                     1       2
+//                            3    4
+
+// 1,3,8,1,11,12,13,1,-1,-1,2,3,-1,-1,4,-1,-1,-1,-1,-1,-1,9,-1,3,-1,2,-1,1,19,-1,-1,14,-1,-1,
+// 2,7,-1,-1,4,-1,-1}
+
+//                                     1
+//                               3            2
+//                             8    9         7   4
+//                            1       3
+//                         11          
+//                         12          
+//                        13           
+//                     1       2
+//                            3    4
+
+// 1,3,8,1,11,12,13,1,-1,-1,2,3,-1,-1,4,-1,-1,-1,-1,-1,-1,9,-1,3,-1,-1,
+// 2,7,-1,-1,4,-1,-1}
