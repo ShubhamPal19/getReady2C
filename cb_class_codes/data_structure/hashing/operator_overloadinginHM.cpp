77c21e2e1722a1e2a0d9e6733ba9fa49a351ac01
ShubhamPal19 <shubhampal2309@gmail.com>
2021-06-11 21:04:58 +0530

    Create operator_overloadinginHM.cpp

---
+++ cb_class_codes/data_structure/hashing/operator_overloadinginHM.cpp
@@ -0,0 +1,197 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+
+using namespace std;
+
+class node
+{
+
+private:
+
+public:
+    string key;
+    int val;
+    node *next;
+    node(string k, int v)
+    {
+
+        val = v;
+        key = k;
+        next = NULL;
+    }
+};
+
+class hashmap
+{
+    int ts;
+    int cs;
+    node **arr;
+
+public:
+
+
+    int hashfunction(string key)
+    {
+        int ans=0;
+        int mul=1;
+        for (int  i = 0; i < key.length(); i++)
+        {
+            ans =  ans + (key[i]%ts)*( mul%ts);
+            mul=(29*mul)%ts;
+
+
+        }
+
+
+        return ans%ts;
+
+        
+    }
+    void rehashing()
+    {
+        node ** oldarr=arr;
+        int oldts=ts,i;
+        arr=(node **)calloc(ts*2,sizeof(node **));
+        ts=2*ts;
+        cs=0;
+        for ( i = 0; i < oldts; i++)
+        {
+            node * head= oldarr[i],*temp;
+            while (head)
+            {
+                int index = hashfunction(head->key);
+       
+                temp=head;
+                head=head->next;
+                arr[index] = temp;
+                temp->next=NULL;
+                cs++;
+       
+            }
+            
+            arr[i]=oldarr[i];
+            if (arr[i])
+            {
+                cs++;
+            }
+            
+        }
+        delete[]oldarr;  
+        
+    }
+
+    hashmap(int size = 7)
+    {
+        cs = 0;
+        ts = size;
+        arr = (node **)calloc(size, sizeof(node **));
+    }
+
+    void insert(string key,int val)
+    {
+        int index = hashfunction(key);
+        node *n = new node(key, val);
+        n->next = arr[index];
+        arr[index] = n;
+        cs++;
+
+        if (cs / ts * 1.0 > 0.6)
+        {
+            rehashing();
+        }
+    }
+
+    void print()
+    {
+        for (int i = 0; i < ts; i++)
+        {
+
+            cout << i <<" --->" ;
+            node *head=arr[i];
+            while (head != NULL)
+            {
+                cout << head->key << "  ";
+                head = head->next;
+            }
+            cout << endl;
+        }
+    }
+
+
+    node * search(string key)
+    {
+        int index=hashfunction(key);
+
+        cout << index << " is index"<<endl;
+        node * head = arr[index];
+        while (head)
+        {
+            if(head->key == key)
+            {
+                return head;
+            }
+            head=head->next;
+        }
+        return NULL;
+
+
+    }
+
+
+    int& operator[](string key)
+    {
+        cout<<"callerfd";
+       node *head = search(key);
+       cout<<"the key is "<< key<<endl;
+       if (head==NULL)
+       {
+          int g=38;
+           insert(key, g);
+       cout<<"the key is "<< key<<endl;
+           head = search(key);
+       cout<<"the key is "<< key<<endl;
+           cout<<" in else";
+           if (head==NULL)
+           {
+               cout<<"NULL";
+           }
+           
+           return head->val;
+       }
+         
+        return head->val;
+    
+    }
+};
+
+
+
+int main()
+{
+    hashmap h;
+
+    h.insert("mango",1200);
+    h.insert("kiwi",1300);
+    h.insert("orange",1500);
+    h.print();
+    cout <<endl;
+    h.insert("jamun",2100);
+    h.insert("banana",4500);
+    h.insert("apple",200);
+    h.insert("lemon",120);
+    h.insert("tomato",400);
+    h.insert("grapes",5200);
+    h.print();
+    cout <<endl;
+
+    cout<<"vlaue of mango is "  <<  h.hashfunction("mango")<<endl;
+    cout<<"vlaue of mango is "  <<  h.hashfunction("mango")<<endl;
+    cout<<"vlaue of PAPAYA is "  <<  h.hashfunction("paPaya")<<endl;
+
+    h["paPaya"]=200;
+  
+
+    h.print();
+}
