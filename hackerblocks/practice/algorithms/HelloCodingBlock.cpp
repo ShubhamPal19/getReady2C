6e2c3284008eded8eb89351a0b83a4d9e9ed5c54
ShubhamPal19 <shubhampal2309@gmail.com>
2021-06-24 19:09:23 +0530

    Create HelloCodingBlock.cpp

---
+++ hackerblocks/practice/algorithms/HelloCodingBlock.cpp
@@ -0,0 +1,19 @@
+// Write a program to print "Hello Coding Blocks !" on the screen.
+
+// Input Format
+// Constraints
+// Output Format
+// Hello Coding Blocks !
+
+// Sample Input
+// Sample Output
+// Hello Coding Blocks !
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout<<"Hello Coding Blocks !";
+    
+}
