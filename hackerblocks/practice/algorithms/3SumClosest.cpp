1aff5382564973327b07e1fbd896aebd0c826dfd
ShubhamPal19 <shubhampal2309@gmail.com>
2021-06-26 20:39:30 +0530

    Update 3SumClosest.cpp

--- hackerblocks/practice/algorithms/3SumClosest.cpp
+++ hackerblocks/practice/algorithms/3SumClosest.cpp
@@ -33,27 +33,75 @@ int main()
     {
         cin>>a[i];
     }
-    int sol = a[0]+a[1]+a[2];
 
-    for (size_t i = 0; i < n-2; i++)
-    {
-      for (size_t j = i+1; j < n-1; j++)
-      {
-         for (size_t k = j+2; k < n; k++)
-         {
-             if (abs(a[i]+a[j]+a[k]-target)<sol)
-             {
-                 sol=a[i]+a[j]+a[k];
-             }
+   
+    // int sol = a[0]+a[1]+a[2];
+
+    // for (size_t i = 0; i < n-2; i++)
+    // {
+    //   for (size_t j = i+1; j < n-1; j++)
+    //   {
+    //      for (size_t k = j+2; k < n; k++)
+    //      {
+    //          if (abs(a[i]+a[j]+a[k]-target)<sol)
+    //          {
+    //              sol=a[i]+a[j]+a[k];
+    //          }
              
-         }
-         
-      }
+    //      }
+    //   }
         
+    // }
+    
+    // function to sort ary
+    for (int i = 0; i < n; i++)
+    {
+        int min_indx=i,j;
+        for ( j = i+1; j < n; j++)
+        {
+            if (a[min_indx]>a[j])
+        {
+            min_indx=j;
+        }
+        
+            
+        }
+        swap(a[i],a[min_indx]);
     }
+    int sum=0;
+    int sol=INT_MAX,j,k;
+    for (size_t i = 0; i < n-1; i++)
+    {
+        
+        
+        j=i+1;
+        k=n-1;
+        while(j<k)
+        {
+            sum=a[i]+a[j]+a[k];
+            if (abs(sum-target) < sol)
+            {
+                sol=sum-target;
+            }
+            
+            
+            if (sum>target)
+        
+             k--;
+            
+            else
+            j++;
+            
+        }
+        
+
+    }
+    
+     
     
 
 
-    cout<< sol<<endl;
+    
+    cout<< sol+target<<endl;
     return 0;
 }
